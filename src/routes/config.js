7ac57f209d952b5ad2f2a6868ea06ad7cb16e991
IreneZjj <1152558897@qq.com>
2019-07-22 20:53:32 +0800

    irene test for push

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,5 +1,5 @@
 export default {
-    menus: [ // 菜单相关路由 irene test
+    menus: [ // 菜单相关路由 irene test!
         { key: '/app/dashboard/index', title: '首页', icon: 'mobile', component: 'Dashboard' },
         {
             key: '/app/ui', title: 'UI', icon: 'scan',
